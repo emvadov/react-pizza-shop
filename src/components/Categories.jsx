ba3fb5b2df1d2a5fa53bafd18f7f8101810915d4
Emil Avadov <emvadov@gmail.com>
2021-05-21 01:55:21 +0300

    Creating Routers

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Categories( {onClickItem, items} ) {
+    const [activeItem, setActiveItem] = React.useState(null)
+
+    const onSelectItem = (index) => {
+      setActiveItem(index)
+    }
+
+
+    return (
+        <div className="categories">
+
+              <ul>
+                <li className={activeItem === null ? 'active' : ' ' }  onClick={() => onSelectItem(null)}>All</li>
+                {items && items.map((name, index) => (
+                    <li 
+                      className={activeItem === index ? 'active' : ' ' } 
+                      onClick={() => onSelectItem(index)} 
+                      key={`${name}_${index}`}>{name}
+                    </li>
+            
+                ))}
+                
+              </ul>
+            </div>
+    )
+    
+}
+
+export default Categories
