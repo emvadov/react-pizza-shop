900f6ccec25a0a064e1d4acbda03e80b9b59022e
Emil Avadov <emvadov@gmail.com>
2021-07-23 17:16:51 +0300

    Added Add to Cart

--- src/redux/actions/pizza.js
+++ src/redux/actions/pizza.js
@@ -1,3 +1,21 @@
+import axios from 'axios'
+
+export const setLoaded = val => ({
+    type: 'SET_LOADING',
+    payload: val
+});
+
+export const fetchPizza = (sortBy, category) => (dispatch) => {
+    
+    dispatch(setLoaded(false));
+    axios.get(`/pizza?${category !== null ? `category=${category}` : '' }&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
+        dispatch(setPizza(data));
+
+        
+      
+    });
+}
+
 export const setPizza = (items) => ({
 
     type: 'SET_PIZZA',
